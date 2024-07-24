13bc5622d23f0b301a3fb30c49a3571703708789
emin.dzanko <emin.dzanko@isatis.com>
2024-07-24 16:19:50 +0200

    Implemented update functionality and loading spinner

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.DTOs;
 using API.Entities;
 using API.Interfaces;
@@ -8,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
@@ -18,7 +19,6 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         return Ok(users);       
     }
     
-    [Authorize]
     [HttpGet("{username}")]
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
@@ -27,5 +27,23 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         if (user == null) return NotFound();
 
         return user; 
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<MemberDto>> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (username == null) return BadRequest("No username found in tokens");
+
+        var user = await userRepository.GetUserByUsernameAsync(username); 
+        
+        if (user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDto, user);
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
     }
 }
